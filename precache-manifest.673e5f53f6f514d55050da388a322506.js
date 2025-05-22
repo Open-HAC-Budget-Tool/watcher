ce46641bad67d1423c479c2b147bdad97eba0cd5
Amanda Shih <pandafulmanda@gmail.com>
2025-05-22 04:51:23 -0500

    Updates

---
+++ precache-manifest.673e5f53f6f514d55050da388a322506.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca3fb2ba6dad680adfe1fd60bbb02a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef67b3f34faf8367fd84",
+    "url": "./static/css/3.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "f69397373cfe6e2a234f",
+    "url": "./static/css/main.7565db1d.chunk.css"
+  },
+  {
+    "revision": "5d9f5f654dbae9070d3d",
+    "url": "./static/js/2.6f5c16fb.chunk.js"
+  },
+  {
+    "revision": "163e9998587691484c1ab309a3b23721",
+    "url": "./static/js/2.6f5c16fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef67b3f34faf8367fd84",
+    "url": "./static/js/3.9bcdd3eb.chunk.js"
+  },
+  {
+    "revision": "26328d4bf17442650586f4961fef5065",
+    "url": "./static/js/3.9bcdd3eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "994bbfddd6673d712354",
+    "url": "./static/js/4.e18fe59d.chunk.js"
+  },
+  {
+    "revision": "8ec78d21f45102a5ab06",
+    "url": "./static/js/5.16912b9f.chunk.js"
+  },
+  {
+    "revision": "9c22045a240dc37226c6",
+    "url": "./static/js/6.65fbec67.chunk.js"
+  },
+  {
+    "revision": "f69397373cfe6e2a234f",
+    "url": "./static/js/main.946858d1.chunk.js"
+  },
+  {
+    "revision": "631408040c51c1f6fd8c",
+    "url": "./static/js/runtime-main.88f378b5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6710d7605316671b3daba0f7930e2aa1.js"
+  "./precache-manifest.673e5f53f6f514d55050da388a322506.js"
 );
 
 self.addEventListener('message', (event) => {
